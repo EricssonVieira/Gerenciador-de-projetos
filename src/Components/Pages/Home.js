5857d1c3ed582c3e5eca5f735bad0442b2c98d00
EricssonVieira <ericssonrvieira@gmail.com>
2022-11-02 16:44:33 -0300

    home page criada

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,7 +1,14 @@
+import saving from "../layouts/img/savings.png";
+import LinkButton from "../layouts/LinkButton";
+import styles from "./Home.module.css";
+
 export default function Home(){
   return(
-    <>
-      <h1>home</h1>
-    </>
+    <section className={styles.homeContainer}>
+      <h1>Bem-Vindo  ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/novoprojeto" text="Criar projeto"/>
+      <img src={saving}/>
+    </section>
   )
 }
